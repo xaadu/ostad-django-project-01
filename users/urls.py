e6bf1c03a1cc9483edbd65b03005191d2a89c631
xaadu <abdullahzayed01@gmail.com>
2024-10-06 12:41:10 +0600

    some basic formatting

--- users/urls.py
+++ users/urls.py
@@ -4,35 +4,41 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('otp-verify/', views.otp_verify_view, name='otp_verify'),
-    path('home/', views.home_view, name='home'),
-    path('logout/', views.logout_view, name='logout'),  # Added logout URL
-    path('profile-update/', views.profile_update_view, name='profile_update'),
-    path('register/', views.register_view, name='register'),  # Registration URL  # Added profile update URL
-
-# Password Reset URLs
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='registration/password_reset_form.html'
-         ),
-         name='password_reset'),
-
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/password_reset_done.html'
-         ),
-         name='password_reset_done'),
-
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
-
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
+    path("login/", views.login_view, name="login"),
+    path("otp-verify/", views.otp_verify_view, name="otp_verify"),
+    path("home/", views.home_view, name="home"),
+    path("logout/", views.logout_view, name="logout"),  # Added logout URL
+    path("profile-update/", views.profile_update_view, name="profile_update"),
+    path(
+        "register/", views.register_view, name="register"
+    ),  # Registration URL  # Added profile update URL
+    # Password Reset URLs
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
